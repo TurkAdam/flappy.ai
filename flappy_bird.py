7f0fac895820cdcc64dece9113882f8e3dc802c5
Niyaz B. Hashem <niyazed@outlook.com>
2019-09-03 02:44:41 +0600

    AI init

--- flappy_bird.py
+++ flappy_bird.py
@@ -6,26 +6,31 @@ import random
 from classes.bird import Bird
 from classes.pipe import Pipe
 from classes.base import Base
+pygame.font.init()
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
-
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
+STAT_FONT = pygame.font.SysFont("gigi", 50)
 
-def draw_window(win, bird, pipes, base):
+def draw_window(win, bird, pipes, base, score):
     win.blit(BG_IMG, (0,0)) #blit means draw EX. win.draw()
 
     for pipe in pipes:
         pipe.draw(win)
 
+    text = STAT_FONT.render("Score: " + str(score), 1, (255,255,255))
+    win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
     base.draw(win)
     bird.draw(win)
     pygame.display.update()
 
 
-def main():
+def main(genomes, config):
+    BASE_POINT = 700
+
     bird = Bird(230,350)
-    base = Base(700)
+    base = Base(BASE_POINT)
     pipes = [Pipe(600)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
@@ -44,8 +49,7 @@ def main():
         for pipe in pipes:
             if pipe.collide(bird):
                 pass 
-
-
+                
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 remove.append(pipe)
 
@@ -62,11 +66,34 @@ def main():
 
         for pipe in remove:
             pipes.remove(pipe)
+
+
+        if bird.y + bird.img.get_height() >= BASE_POINT:
+            pass
+
         base.move()
-    
-        draw_window(win, bird, pipes, base)
+        draw_window(win, bird, pipes, base, score)
 
     pygame.quit()
     quit()
 
 main()
+
+def run(config_file):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+
+    population = neat.Population(config)
+
+    population.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    population.add_reporter(stats)
+
+    winner = population.run(main, 50)
+
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__) # get the current directory
+    config_path = os.path.join(local_dir, "neat-config.txt")
+    run(config_path)
